9d21bf3262f3253d2c7807e3737ce069803d8afc
llpplj1001 <juyu7@s.sfusd.edu>
2026-05-21 13:37:45 -0700

    character (finish)

--- Scripts/background_parallax.gd
+++ Scripts/background_parallax.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var parallax : float = 0.7
-@onready var player = $"../Player"
+@onready var player = $"../character"
 
 func _process(delta):
 	global_position = player.global_position * parallax
