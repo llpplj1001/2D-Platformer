6e1cd83b625f3d01af0224b4404d0fc2ba09bf4a
Jasongda <jayu16@s.sfusd.edu>
2026-05-26 14:19:05 -0700

    Added enemies, tiles

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -4,7 +4,7 @@ extends Control
 
 func _on_player_button_pressed() -> void:
 	PlayerStats.score = 0
-	get_tree().change_scene_to_file("res://Scenes/Level_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level 1 testing.tscn")
 
 
 func _on_quit_button_2_pressed() -> void:
