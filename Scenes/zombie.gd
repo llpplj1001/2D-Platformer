896f3d314b5e025cc3bd5406ccd02d4e21a8f089
Jasongda <jayu16@s.sfusd.edu>
2026-05-27 16:46:26 -0700

    Boss,enemies,coins level 3

--- Scenes/zombie.gd
+++ Scenes/zombie.gd
@@ -17,11 +17,11 @@ func _physics_process(delta):
 
 		if target_pos == start_pos:
 			target_pos = start_pos + move_direction
-			$zombie.flip_h = true
+			$zombie.flip_h = false
 
 		else:
 			target_pos = start_pos
-			$zombie.flip_h = false
+			$zombie.flip_h = true
 
 
 func _on_body_entered(body):
